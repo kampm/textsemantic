4157eb2ed0fd98382a20a7640041a44e0e26d03e
kampm <kam9@protonmail.com>
2021-03-10 21:00:58 +0100

    maskLM oom/ Bert NER (trained in kaggle)

--- BRET_transformers_testpy.py
+++ BRET_transformers_testpy.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+-*- coding: utf-8 -*-
 """
 Created on Mon Feb 15 18:06:45 2021
 
@@ -50,11 +50,319 @@ for pred in nlp_ner(f"Adam Mickiewicz wielkim polskim {nlp_ner.tokenizer.mask_to
   print(pred)
 
 
-# 3CosAdd “what is to Portugal as Paris is to France?”. 
-# b∗ = argmax cos(w, a∗ − a + b) 
-# w∈V
+3CosAdd “what is to Portugal as Paris is to France?”. 
+b∗ = argmax cos(w, a∗ − a + b) 
+w∈V
 
-# 3CosMul
-# b∗ = argmax ( cos(b, w) × cos(w, a∗) / cos(w, a) )
-# w∈V
+3CosMul
+b∗ = argmax ( cos(b, w) × cos(w, a∗) / cos(w, a) )
+w∈V
 
+
+# not working!
+# MAX_LENGTH = 120 #@param {type: "integer"}
+# MODEL = "roberta_base"
+
+# import sys
+
+# from transformers import AutoTokenizer
+
+# dataset = "train_temp.txt"
+# model_name_or_path = MODEL
+# max_len = 120
+
+# subword_len_counter = 0
+
+# import torch, os
+# from transformers import RobertaModel, AutoModel, PreTrainedTokenizerFast
+# tokenizer = PreTrainedTokenizerFast(tokenizer_file=os.path.join(MODEL, "tokenizer.json"))
+
+
+# def load_data(filename: str):
+#     with open(filename, 'r', encoding="utf-8") as file:
+#         lines = [line[:-1].split() for line in file if "-DOCSTART" not in line]
+#     samples, start = [], 0
+#     for end, parts in enumerate(lines):
+#         if not parts:
+#             try:
+#                 sample = [(token, tag)
+#                           for token, tag in lines[start:end]]
+#             except:
+#                 print(lines[start:end])
+#             samples.append(sample)
+#             start = end + 1
+#     if start < end:
+#         samples.append(lines[start:end])
+#     return samples
+
+
+# train_samples = load_data(dataset)
+# val_samples = train_samples[:8000]# test
+
+# samples = train_samples 
+# schema = ['_'] + sorted({tag for sentence in samples
+#                          for _, tag in sentence})
+
+# import tqdm
+# import numpy as np
+# def tokenize_sample(sample):
+#     seq = [
+#         (subtoken, tag)
+#         for token, tag in sample
+#         for subtoken in tokenizer(token)['input_ids'][1:-1]
+#     ]
+#     return [(3, 'O')] + seq + [(4, 'O')]
+
+
+# def preprocess(samples):
+#     tag_index = {tag: i for i, tag in enumerate(schema)}
+#     tokenized_samples = list(map(tokenize_sample, samples))
+#     max_len = max(map(len, tokenized_samples))
+#     X = np.zeros((len(samples), max_len), dtype=np.int32)
+#     y = np.zeros((len(samples), max_len), dtype=np.int32)
+#     for i, sentence in enumerate(tokenized_samples):
+#         for j, (subtoken_id, tag) in enumerate(sentence):
+#             X[i, j] = subtoken_id
+#             y[i, j] = tag_index[tag]
+#     return X, y
+
+
+# X_train, y_train = preprocess(train_samples)
+# X_val, y_val = preprocess(val_samples)
+
+# print(X_train.shape)
+# print(y_train.shape)
+# X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+# y_train = np.reshape(y_train, (y_train.shape[0], 1, y_train.shape[1]))
+
+# import pandas as pd
+
+# EPOCHS=10
+# BATCH_SIZE=16
+
+# import tensorflow as tf
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Bidirectional, LSTM, TimeDistributed, Dense
+
+
+# def build_model(nr_filters=256):
+#     input_shape = (1, 141)
+#     # lstm = LSTM(nr_filters, return_sequences=True)
+#     lstm =LSTM(units=50, input_shape=(1, 141), return_sequences=True)
+#     bi_lstm = Bidirectional(lstm, input_shape=input_shape)
+#     tag_classifier = Dense(len(schema), activation='softmax')
+#     sequence_labeller = TimeDistributed(tag_classifier)
+#     return Sequential([bi_lstm, sequence_labeller])
+
+# model = build_model()
+# model.summary()
+
+
+
+# optimizer = tf.keras.optimizers.Adam(lr=0.00001)
+# loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# model.compile(optimizer=optimizer, loss=loss, metrics='accuracy')
+# history = model.fit(tf.constant(X_train), tf.constant(y_train),
+#                     validation_split=0.2, epochs=EPOCHS, 
+#                     batch_size=BATCH_SIZE)
+
+
+
+
+##NERDA it should work but it gives OOM (4gb mem)
+#it works here -> https://www.kaggle.com/kkkkkkk880/ner-bert-base-polish-cased-v1/
+# !!!!!!!!!!!!!!!!!!
+
+count=0
+countdoc=0
+tabtrain=[]
+tabtest=[]
+tabvalid=[]
+with open("nkjp_ner/nkjp-nested-simplified-v2.txt", encoding="utf-8") as fp:
+    Lines = fp.readlines()
+    for line in Lines:
+        count += 1
+        if "-DOCSTART" in line:
+            countdoc += 1
+            line = ""
+        if countdoc<3200:
+            tabtrain.append(line.replace("	"," ").replace("#"," "))
+        
+        elif 3000<countdoc<3700:
+            tabtest.append(line.replace("	"," ").replace("#"," "))
+        elif countdoc>3700:
+            tabvalid.append(line.replace("	"," ").replace("#"," "))
+    
+fa = open(r"nkjp_ner/train.txt","w+", encoding="utf-8")
+fa.write("".join(tabtrain))
+fa.close()
+
+fa = open(r"nkjp_ner/valid.txt","w+", encoding="utf-8")
+fa.write("".join(tabvalid))
+fa.close()
+
+fa = open(r"nkjp_ner/test.txt","w+", encoding="utf-8")
+fa.write("".join(tabtest))
+fa.close()
+
+
+from NERDA.datasets import get_dane_data,get_conll_data, download_conll_data 
+
+
+
+
+# download_conll_data()
+training = get_conll_data('train')
+validation = get_conll_data('valid')
+test = get_conll_data('test')
+training["sentences"][0]
+
+
+unique_data = list(map(list, set(map(lambda i: tuple(i), test["tags"]))))
+result = list(set(x for l in unique_data for x in l))
+for r in result:
+    print(f'"{r}",')
+    
+unique_data = list(map(list, set(map(lambda i: tuple(i), test["tags"]))))
+result2 = list(set(x for l in unique_data for x in l))
+
+tag_scheme = [
+"B-persName-forename",
+"I-placeName-bloc",
+"B-orgName",
+"I-placeName-region",
+"B-persName-surname",
+"B-placeName",
+"B-date",
+"B-time",
+"I-persName",
+"B-geogName",
+"I-placeName-settlement",
+"I-date",
+"I-persName-surname",
+"B-persName",
+"B-placeName-region",
+"I-geogName",
+"I-orgName",
+"B-placeName-country",
+"I-persName-forename",
+"I-time",
+"I-placeName-country",
+"B-placeName-bloc",
+"I-persName-addName",
+"B-placeName-district",
+"B-placeName-settlement",
+"I-placeName-district",
+"B-persName-addName"
+]
+
+transformer = 'dkleczek/bert-base-polish-cased-v1'# only bert small models/roberta OOM
+
+
+
+# hyperparameters for network
+dropout = 0.1
+# hyperparameters for training
+training_hyperparameters = {
+'epochs' : 1,'warmup_steps' : 500,'train_batch_size': 1,'learning_rate': 0.0001
+}
+
+from NERDA.models import NERDA
+model = NERDA(
+dataset_training = training,
+dataset_validation = validation,
+tag_scheme = tag_scheme, 
+tag_outside = 'O',
+transformer = transformer,
+dropout = dropout,
+hyperparameters = training_hyperparameters
+)
+
+model.train()
+
+# test = get_conll_data('test')
+# model.evaluate_performance(test)
+
+#eng train data on pl bert
+# Level	F1-Score
+# 0	B-PER	0.884507
+# 1	I-PER	0.941590
+# 2	B-ORG	0.771963
+# 3	I-ORG	0.751755
+# 4	B-LOC	0.864624
+# 5	I-LOC	0.704762
+# 6	B-MISC	0.738714
+# 7	I-MISC	0.570136
+# 0	AVG_MICRO	0.825114
+# 0	AVG_MACRO	0.778506
+
+#pl train data on pl bert (eval-vaild.txt)
+e1 Train Loss = 0.26577291781040807 Valid Loss = 0.12885032495519444
+e2 Train Loss = 0.08480613472856427 Valid Loss = 0.10683761777700092
+e3 Train Loss = 0.04463438292757332 Valid Loss = 0.11588288823524333
+e4 Train Loss = 0.024082582885794748 Valid Loss = 0.11854389137669845
+e5 Train Loss = 0.011541870708788758 Valid Loss = 0.11877383219411418
+# 	Level	F1-Score
+# 0	B-persName	0.673611
+# 1	B-placeName-bloc	0.125000
+# 2	B-persName-forename	0.917184
+# 3	I-date	0.953954
+# 4	B-placeName-region	0.686391
+# 5	I-persName-forename	0.797688
+# 6	B-placeName	0.861702
+# 7	B-geogName	0.631502
+# 8	B-persName-surname	0.915133
+# 9	I-placeName-settlement	0.733813
+# 10	I-placeName-bloc	0.000000
+# 11	I-placeName	0.000000
+# 12	B-date	0.925566
+# 13	I-placeName-region	0.861925
+# 14	I-persName-addName	0.642857
+# 15	B-placeName-country	0.901505
+# 16	I-placeName-district	0.230769
+# 17	I-persName-surname	0.905660
+# 18	B-persName-addName	0.262774
+# 19	I-time	0.925490
+# 20	I-orgName	0.772438
+# 21	B-placeName-settlement	0.872242
+# 22	I-geogName	0.682051
+# 23	B-time	0.852459
+# 24	I-persName	0.801724
+# 25	I-placeName-country	0.691589
+# 26	B-placeName-district	0.383838
+# 27	B-orgName	0.739605
+# 0	AVG_MICRO	0.836549
+# 0	AVG_MACRO	0.669588
+
+#pl train data on pl bert (eval-test.txt)
+# Level	F1-Score
+# 0	B-persName	0.673913
+# 1	B-placeName-bloc	0.333333
+# 2	B-persName-forename	0.873257
+# 3	I-date	0.854881
+# 4	B-placeName-region	0.500000
+# 5	I-persName-forename	0.142857
+# 6	B-placeName	0.769231
+# 7	B-geogName	0.457143
+# 8	B-persName-surname	0.895307
+# 9	I-placeName-settlement	0.571429
+# 10	I-placeName-bloc	0.000000
+# 11	I-placeName	0.000000
+# 12	B-date	0.753846
+# 13	I-placeName-region	1.000000
+# 14	I-persName-addName	0.000000
+# 15	B-placeName-country	0.916808
+# 16	I-placeName-district	0.000000
+# 17	I-persName-surname	0.000000
+# 18	B-persName-addName	0.202532
+# 19	I-time	0.847059
+# 20	I-orgName	0.809524
+# 21	B-placeName-settlement	0.750000
+# 22	I-geogName	0.473684
+# 23	B-time	0.813953
+# 24	I-persName	0.564103
+# 25	I-placeName-country	0.666667
+# 26	B-placeName-district	0.500000
+# 27	B-orgName	0.700201
+# 0	AVG_MICRO	0.795547
+# 0	AVG_MACRO	0.538205
--- maskLM_testpy.py
+++ maskLM_testpy.py
@@ -5,8 +5,14 @@ Created on Thu Mar  4 15:20:18 2021
 @author: Kamil
 """
 
+# RoBERTa has exactly the same architecture as BERT. The only differences are:
+
+# RoBERTa uses a Byte-Level BPE tokenizer with a larger subword vocabulary (50k vs 32k).
+# RoBERTa implements dynamic word masking and drops next sentence prediction task.
+# RoBERTa's training hyperparameters.
+
 import torch
-from transformers import RobertaForMaskedLM,LineByLineTextDataset,RobertaTokenizer, BertTokenizer, BertModel, BertForMaskedLM, AutoModel, AutoTokenizer, AutoModelWithLMHead, ElectraModel,PreTrainedModel ,ElectraForMaskedLM
+from transformers import RobertaForMaskedLM,LineByLineTextDataset,RobertaTokenizer, BertTokenizer, BertModel, BertForMaskedLM, AutoModel, AutoTokenizer, AutoModelWithLMHead,AutoModelForMaskedLM, ElectraModel,PreTrainedModel ,ElectraForMaskedLM
 from transformers import Trainer,logging, TrainingArguments,pipeline,LineByLineTextDataset,DataCollatorForLanguageModeling
 MODEL_PATH = 'roberta_base'
 
@@ -132,3 +138,341 @@ fill_mask("Sześć osób <mask> w hokeja na lodzie .")
 #train=CDS_corpus,block_size=256,epochs=5,batch=4
 # <mask> w czerwonej bluzie stoi przy barierce nad wodą , a obok niego stoi oparta wędka . = Człowiek,Para,▁mężczyzna,Ludzie,▁Mężczyzna
 #Sześć osób <mask> w hokeja na lodzie . = _gra,_uprawia,_grają,skakuje,łuje
+
+
+##################################################wiki
+from tokenizers import ByteLevelBPETokenizer
+
+path = "roberta_test/train.txt" #plwiki
+
+# Initialize a tokenizer
+tokenizer = ByteLevelBPETokenizer()
+
+# Customize training
+tokenizer.train(files=path,
+                vocab_size=50265,
+                min_frequency=5,
+                special_tokens=["<s>", "<pad>", "</s>", "<unk>", "<mask>"])
+
+
+
+tokenizer.save("roberta_test/tokenizer.json")
+
+import json
+config = {
+	"architectures": [
+		"RobertaForMaskedLM"
+	],
+	"attention_probs_dropout_prob": 0.1,
+	"hidden_act": "gelu",
+	"hidden_dropout_prob": 0.1,
+	"hidden_size": 768,
+	"initializer_range": 0.02,
+	"intermediate_size": 3072,
+	"layer_norm_eps": 1e-05,
+	"max_position_embeddings": 514,
+	"model_type": "roberta",
+	"num_attention_heads": 12,
+	"num_hidden_layers": 12,
+	"type_vocab_size": 1,
+	"vocab_size": 50265
+}
+
+with open("roberta_test/config.json", 'w') as fp:
+    json.dump(config, fp)
+
+tokenizer_config = {"max_len": 126}
+
+with open("roberta_test/tokenizer_config.json", 'w') as fp:
+    json.dump(tokenizer_config, fp)
+    
+MODEL_TYPE = "roberta" #@param ["roberta", "bert"]
+MODEL_DIR = "roberta_test" #@param {type: "string"}
+OUTPUT_DIR = "roberta_test/output" #@param {type: "string"}
+TRAIN_PATH = "roberta_test/train.txt" #@param {type: "string"}
+EVAL_PATH = "roberta_test/dev.txt" #@param {type: "string"}
+
+
+# cmd = """python run_language_modeling.py \
+#     --output_dir {output_dir} \
+#     --model_type {model_type} \
+#     --mlm \
+#     --config_name {config_name} \
+#     --tokenizer_name {tokenizer_name} \
+#     {line_by_line} \
+#     {should_continue} \
+#     {model_name_or_path} \
+#     --train_data_file {train_path} \
+#     --eval_data_file {eval_path} \
+#     --do_train \
+#     {do_eval} \
+#     {evaluate_during_training} \
+#     --overwrite_output_dir \
+#     --block_size 126 \
+#     --max_step 25 \
+#     --warmup_steps 10 \
+#     --learning_rate 5e-5 \
+#     --per_gpu_train_batch_size 1 \
+#     --gradient_accumulation_steps 4 \
+#     --weight_decay 0.01 \
+#     --adam_epsilon 1e-6 \
+#     --max_grad_norm 100.0 \
+#     --save_total_limit 10 \
+#     --save_steps 10 \
+#     --logging_steps 2 \
+#     --seed 42
+# """
+
+cmd = """python run_mlm.py \
+    --output_dir {output_dir} \
+    --model_type {model_type} \
+    --config_name {config_name} \
+    --tokenizer_name {tokenizer_name} \
+    {line_by_line} \
+    {should_continue} \
+    {model_name_or_path} \
+    --train_file {train_path} \
+    --validation_file {eval_path} \
+    --do_train \
+    {do_eval} \
+    {evaluate_during_training} \
+    --overwrite_output_dir \
+    --max_seq_length 126 \
+    --max_step 25 \
+    --warmup_steps 10 \
+    --learning_rate 5e-5 \
+    --per_gpu_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --weight_decay 0.01 \
+    --adam_epsilon 1e-6 \
+    --max_grad_norm 100.0 \
+    --save_total_limit 10 \
+    --save_steps 10 \
+    --logging_steps 2 \
+    --seed 42
+"""
+
+train_params = {
+    "output_dir": OUTPUT_DIR,
+    "model_type": MODEL_TYPE,
+    "config_name": MODEL_DIR,
+    "tokenizer_name": MODEL_DIR,
+    "train_path": TRAIN_PATH,
+    "eval_path": EVAL_PATH,
+    "do_eval": "--do_eval",
+    "evaluate_during_training": "",
+    "line_by_line": "",
+    "should_continue": "",
+    "model_name_or_path": "",
+}
+
+!{cmd.format(**train_params)}
+
+
+from transformers import pipeline
+
+fill_mask = pipeline(
+    "fill-mask",
+    model="roberta_test/output",
+    tokenizer="roberta_test/output"
+)
+
+fill_mask("interpretowany język <mask>.")
+
+#########################################Extract-text-from-WolneLekturyPL.ipynb
+
+import json
+from pathlib import Path
+from glob import glob
+import os
+from concurrent.futures import ProcessPoolExecutor
+from itertools import chain
+import nltk
+import re
+nltk.download('punkt')
+
+extra_abbreviations = ['ps',  'inc', 'Corp', 'Ltd', 'Co', 'pkt', 'Dz.Ap', 'Jr', 'jr', 'sp', 'Sp', 'poj',  'pseud', 'krypt', 'sygn', 'Dz.U', 'ws', 'itd', 'np', 'sanskryt', 'nr', 'gł', 'Takht', 'tzw', 't.zw', 'ewan', 'tyt', 'oryg', 't.j', 'vs', 'l.mn', 'l.poj' ]
+
+position_abbrev = ['Ks', 'Abp', 'abp','bp','dr', 'kard', 'mgr', 'prof', 'zwycz', 'hab', 'arch', 'arch.kraj', 'B.Sc', 'Ph.D', 'lek', 'med', 'n.med', 'bł', 'św', 'hr', 'dziek' ]
+
+quantity_abbrev = [ 'mln', 'obr./min','km/godz', 'godz', 'egz', 'ha', 'j.m', 'cal', 'obj', 'alk', 'wag' ] # not added: tys.
+
+actions_abbrev = ['tłum','tlum','zob','wym', 'pot', 'ww', 'ogł', 'wyd', 'min', 'm.i', 'm.in', 'in', 'im','muz','tj', 'dot', 'wsp', 'właść', 'właśc', 'przedr', 'czyt', 'proj', 'dosł', 'hist', 'daw', 'zwł', 'zaw' ]
+
+place_abbrev = ['Śl', 'płd', 'geogr']
+
+lang_abbrev = ['jęz','fr','franc', 'ukr', 'ang', 'gr', 'hebr', 'czes', 'pol', 'niem', 'arab', 'egip', 'hiszp', 'jap', 'chin', 'kor', 'tyb', 'wiet', 'sum']
+
+military_abbrev = ['kpt', 'kpr', 'obs', 'pil', 'mjr','płk', 'dypl', 'pp', 'gw', 'dyw', 'bryg', 'ppłk', 'mar', 'marsz', 'rez', 'ppor', 'DPanc', 'BPanc', 'DKaw', 'p.uł']
+
+extra_abbreviations= extra_abbreviations + position_abbrev + quantity_abbrev + place_abbrev + actions_abbrev + place_abbrev + lang_abbrev+military_abbrev
+
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/polish.pickle')
+sentence_tokenizer._params.abbrev_types.update(extra_abbreviations)
+sent_tokenize = sentence_tokenizer.tokenize
+
+def flatten(iterable):
+    return chain.from_iterable(iterable)
+
+def preprocess_book(book_txt):
+    if "404 Not Found" in book_txt:
+        return ""
+    
+    start_idx = book_txt.index("\n" * 4) + 5
+    end_idx = book_txt.index("-----") - 5
+    txt =  book_txt[start_idx: end_idx]
+    return re.sub("\s+", " ", txt)
+
+def process_book(book_path):
+    
+    try:
+        txt = preprocess_book(Path(book_path).read_text("utf-8"))
+        sentences = [s for s in sent_tokenize(txt) if len(s) >= 16]
+        windowed_sentences = []
+        for snt in range(len(sentences)):
+            windowed_sentences.append(" ".join(sentences[snt: snt + 8]))
+        return windowed_sentences
+    except:
+        print(f"Could not parse \n{book_path}\n")
+        return []
+
+books = list(glob("wolne-lektury/*.txt"))
+books[:10]
+
+buffer, BUFFER_SIZE = [], 100000
+with open("wolne-lektury.sliding8.txt", "wt",encoding="utf-8") as file:
+    for i, sentence in enumerate(flatten(process_book(f) for f in books)):
+        if len(buffer) >= BUFFER_SIZE:
+            file.write("\n".join(buffer))
+            buffer.clear()
+            print(i, end="\r")
+        buffer.append(sentence)
+    if len(buffer) > 0:
+        file.write("\n".join(buffer))
+        buffer.clear()
+     
+        
+     
+from pathlib import Path
+
+from tokenizers import ByteLevelBPETokenizer
+from glob import glob
+paths = list(
+    glob("wolne-lektury.sliding8.txt")
+)
+# Initialize a tokenizer
+tokenizer = ByteLevelBPETokenizer(lowercase=False)
+
+# Customize training
+tokenizer.train(files=paths, vocab_size=32000, min_frequency=3, special_tokens=[
+    "<s>",
+    "<pad>",
+    "</s>",
+    "<unk>",
+    "<mask>",
+])
+
+
+
+tokenizer.save("wolne-lektury/tokenizer.json")
+
+import json
+config = {
+	"architectures": [
+		"RobertaForMaskedLM"
+	],
+	"attention_probs_dropout_prob": 0.1,
+	"hidden_act": "gelu",
+	"hidden_dropout_prob": 0.1,
+	"hidden_size": 768,
+	"initializer_range": 0.02,
+	"intermediate_size": 3072,
+	"layer_norm_eps": 1e-05,
+	"max_position_embeddings": 514,
+	"model_type": "roberta",
+	"num_attention_heads": 12,
+	"num_hidden_layers": 12,
+	"type_vocab_size": 1,
+	"vocab_size": 32000
+}
+
+with open("wolne-lektury/config.json", 'w') as fp:
+    json.dump(config, fp)
+
+tokenizer_config = {"max_len": 126}
+
+with open("wolne-lektury/tokenizer_config.json", 'w') as fp:
+    json.dump(tokenizer_config, fp)
+
+# Save files to disk
+# import os
+# OUT_DIR = "polish_tokenizer_bpe_32k"
+# os.makedirs(OUT_DIR, exist_ok=True)
+# tokenizer.save(OUT_DIR, "pl")
+MODEL_TYPE = "roberta" #@param ["roberta", "bert"]
+MODEL_DIR = "wolne-lektury" #@param {type: "string"}
+OUTPUT_DIR = "wolne-lektury/output" #@param {type: "string"}
+TRAIN_PATH = "wolne-lektury/wolne-lektury.sliding8.txt" #@param {type: "string"}
+EVAL_PATH = "wolne-lektury/dev.txt" #@param {type: "string"}
+
+cmd = """python run_mlm.py \
+    --output_dir {output_dir} \
+    --model_type {model_type} \
+    --config_name {config_name} \
+    --tokenizer_name {tokenizer_name} \
+    {line_by_line} \
+    {should_continue} \
+    {model_name_or_path} \
+    --train_file {train_path} \
+    --validation_file {eval_path} \
+    --do_train \
+    {do_eval} \
+    {evaluate_during_training} \
+    --overwrite_output_dir \
+    --max_seq_length 126 \
+    --max_step 25 \
+    --warmup_steps 1000 \
+    --learning_rate 5e-5 \
+    --per_gpu_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --weight_decay 0.01 \
+    --adam_epsilon 1e-6 \
+    --max_grad_norm 100.0 \
+    --save_total_limit 10 \
+    --save_steps 10 \
+    --logging_steps 2 \
+    --seed 42
+"""
+
+train_params = {
+    "output_dir": OUTPUT_DIR,
+    "model_type": MODEL_TYPE,
+    "config_name": MODEL_DIR,
+    "tokenizer_name": MODEL_DIR,
+    "train_path": TRAIN_PATH,
+    "eval_path": EVAL_PATH,
+    "do_eval": "--do_eval",
+    "evaluate_during_training": "",
+    "line_by_line": "",
+    "should_continue": "",
+    "model_name_or_path": "",
+}
+
+!{cmd.format(**train_params)}
+
+
+from transformers import pipeline
+
+fill_mask = pipeline(
+    "fill-mask",
+    model="roberta_test/output",
+    tokenizer="roberta_test/output"
+)
+
+# fill_mask("Poszedł i nawet zdał <mask> do Szkoły Głównej")
+# predicted words: siatk,*,wskaźników,kolory,lizmu
+
+
+#https://zablo.net/blog/post/training-roberta-from-scratch-the-missing-guide-polish-language-model/
+#the result for the argument max_seq_length-126 gives very weak predictions
+# allplwiki requires 500gb ram/ for more sequences(max_seq_length)  you need much more gpu memory at least 8gb
